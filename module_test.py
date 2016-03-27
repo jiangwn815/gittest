231ff6cc91e13f41baa37acefd93c733cc0cfca0
jiang <jiangwn815@hotmail.com>
2016-03-28 00:02:59 +0800

    socket test

--- module_test.py
+++ module_test.py
@@ -3,6 +3,7 @@
 import os
 import struct 
 import hashlib
+import socket
 
 def struct_test():
 	with open('./ddd.jpg','rb') as f:
@@ -23,5 +24,28 @@ def hashlib_test():
 	md2.update('123456123456'.encode('utf-8'))
 	print('123456123456 md5:',md5.hexdigest())
 	print('dict value:',ll)
-	
-hashlib_test()
+
+def socket_test():
+	#AF_INET 指的事IPv4协议
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.connect(('www.baidu.com',80))
+	s.send(b'GET / HTTP/1.1\r\nHost: www.baidu.com.cn\r\nConnection: close\r\n\r\n')
+	buffer = []
+	while True:
+		d = s.recv(1024)
+		if d:
+			buffer.append(d)
+		else:
+			break
+	data = b''.join(buffer)
+	s.close()
+	header, html = data.split(b'\r\n\r\n', 1)
+	print(header.decode('utf-8'))
+	# 把接收的数据写入文件:
+	with open('baidu.html', 'wb') as f:
+		f.write(html)
+
+dd = []
+print(dd)
+socket_test()
+#hashlib_test()

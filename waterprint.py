c7b33b6511a6186021c8d800677dbf7232900e7a
jiangwn815 <jiangwn815@hotmail.com>
2016-04-07 17:33:44 +0800

    update for waterprint

--- waterprint.py
+++ waterprint.py
@@ -2,18 +2,24 @@
 #-*- coding:utf-8 -*-
 
 import os,sys
+
 from PIL import Image,ImageFilter,ImageFont,ImageDraw
 
-def waterprint(filename):
+def waterprint(filename,text="waterprint",font_type="lhandw",font_size=120):
     try:
+        print("Opening "+filename+"......")
         im = Image.open(filename)
         w,h = im.size
-        font1 = ImageFont.truetype('Arial.ttf',120)
+        print(os.path.splitext(filename)[0]+" size:%s x %s" % (w,h))
+        
+        font_type = "C:\\Windows\\Fonts\\"+font_type+".ttf"
+        font1 = ImageFont.truetype(font_type,font_size)
         draw = ImageDraw.Draw(im)
-        draw.text((w*4/5,h*4/5),"JJJJJ",font = font1,fill = (0,255,255))
-        im.save(filename+"_120.jpg","jpeg")
+        draw.text((w*4/5,h*4/5),text,font = font1,fill = (0,255,255))
+        
+        im.save(filename+"_120.jpg","jpeg",quality=100)
     except IOError:
         print('Cannot open the file')
     
 if __name__ == "__main__":
-    waterprint("DSC01950.JPG")
+    waterprint("DSC01950.JPG","JF@Austrulia")
